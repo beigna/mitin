6a10a9668ad71134c90dae8146a55cd6278f8869
nachopro <tranceway@gmail.com>
2011-07-27 20:46:25 -0300

    Some went worng :P

--- source/audits/models.py
+++ source/audits/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Audit(models.Model):
 
-    created_at = models.DateField(auto_now_add=True)
-    updated_at = models.DateField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
     class Meta:
