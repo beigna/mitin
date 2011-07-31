8ea4b4c2f53f0f635935d02be79e939a81011087
nachopro <tranceway@gmail.com>
2011-07-30 23:50:34 -0300

    Some features are working but signals throws emails in every saves. This must be solved... another day :P

--- source/meetings/views.py
+++ source/meetings/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.core.urlresolvers import reverse
 from django.forms.formsets import formset_factory
+from django.forms.models import modelformset_factory
 
 from meetings.models import Meeting, Guest
 from meetings.forms import MeetingForm, GuestForm
@@ -36,21 +37,28 @@ def confirm(request):
         fakeid=request.GET.get('f'),
         key=request.GET.get('k'))
 
+    if meeting.is_confirmed:
+        return redirect(reverse('meetings_list'))
 
     if request.method == 'GET':
         form = MeetingForm(instance=meeting)
-        GuestFormSet = formset_factory(GuestForm, extra=5)
+        GuestFormSet = modelformset_factory(Guest, fields=('email',), extra=5)
         guest_forms = GuestFormSet()
 
     elif request.method == 'POST':
         form = MeetingForm(instance=meeting, data=request.POST)
-        GuestFormSet = formset_factory(GuestForm)
+        GuestFormSet = modelformset_factory(Guest, fields=('email',))
         guest_forms = GuestFormSet(request.POST)
 
-        for gf in guest_forms:
-            print gf.fields
-            break
+        if form.is_valid() and guest_forms.is_valid():
+            form.save()
+            instances = guest_forms.save(commit=False)
 
+            for instance in instances:
+                instance.meeting = meeting
+                instance.save()
+
+            return redirect(reverse('meetings_list'))
 
     return render(request, 'meetings/confirm.html',
         {
