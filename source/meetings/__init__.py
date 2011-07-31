573ee0429d4539e58248ecb677c3a5f3d2078669
nachopro <tranceway@gmail.com>
2011-07-30 21:00:58 -0300

    Signal for meeting confirm and some templates enhancement

--- source/meetings/__init__.py
+++ source/meetings/__init__.py
@@ -1,7 +1,8 @@
 
 from django.db.models.signals import post_save
 
-from meetings.models import Guest
-from meetings.signals_handlers import email_notify
+from meetings.models import Meeting, Guest
+from meetings.signals_handlers import email_owner_notify, email_guest_notify
 
-post_save.connect(email_notify, sender=Guest)
+post_save.connect(email_guest_notify, sender=Guest)
+post_save.connect(email_owner_notify, sender=Meeting)
--- source/meetings/signals_handlers.py
+++ source/meetings/signals_handlers.py
@@ -4,11 +4,21 @@ from django.core.mail import send_mail
 from django.template.loader import render_to_string
 
 
-def email_notify(sender, **kwargs):
+def email_owner_notify(sender, **kwargs):
+    meeting = kwargs['instance']
+
+    subject = u'Confirma tu evento'
+    message = render_to_string('emails/confirm.html',
+        {'meeting': meeting})
+
+    send_mail(subject=subject, message=message,
+        from_email=settings.EMAIL_SENDER, recipient_list=[meeting.owner_email])
+
+def email_guest_notify(sender, **kwargs):
     guest = kwargs['instance']
 
     subject = u'Te invitaron a %s' % guest.meeting.where
-    message = render_to_string('emails/guest_confirm.html', {'guest': guest})
+    message = render_to_string('emails/respond.html', {'guest': guest})
 
     send_mail(subject=subject, message=message,
         from_email=settings.EMAIL_SENDER, recipient_list=[guest.email])
